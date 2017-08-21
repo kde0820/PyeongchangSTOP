80d220ec9886a26518a8c97d74beeee11f02724f
kde0820 <kde0820@naver.com>
2017-08-22 00:56:56 +0900

    add Quiz

--- app/src/main/java/com/example/daeun/pyeongchangstop/OxQuizActivity.java
+++ app/src/main/java/com/example/daeun/pyeongchangstop/OxQuizActivity.java
@@ -3,11 +3,11 @@ package com.example.daeun.pyeongchangstop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PointActivity extends AppCompatActivity {
+public class OxQuizActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_point);
+        setContentView(R.layout.activity_ox_quiz);
     }
 }
